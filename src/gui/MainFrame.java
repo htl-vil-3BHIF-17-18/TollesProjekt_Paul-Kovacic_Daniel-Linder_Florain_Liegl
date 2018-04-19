0c22de8e889f459b1558481174c6174b0469d90f
Florian <Florian@FLO-PC>
2018-04-19 15:42:32 +0200

    gui

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MainFrame extends JFrame{
 
@@ -11,6 +13,18 @@ public class MainFrame extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = -8026416994513756565L;
+	private JMenu file;
+	private JMenuItem settings;
+	
+	private JMenu edit;
+	
+	private JMenu connection;
+	private JMenuItem login;
+	private JMenuItem logout;
+	
+	private JMenu window;
+	private JMenuItem preferences;
+	
 	public MainFrame(String identifier) throws HeadlessException {
 		super(identifier);
 		this.setMinimumSize(new Dimension(1920, 1080));
