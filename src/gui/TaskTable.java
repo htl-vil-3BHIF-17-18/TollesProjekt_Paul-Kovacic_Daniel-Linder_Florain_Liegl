09d8fc7ecf930fd146d779ef2fadf7f4c7b69296
Florian <Florian@FLO-PC>
2018-04-23 09:50:17 +0200

    do

--- src/gui/TaskTable.java
+++ src/gui/TaskTable.java
@@ -34,6 +34,7 @@ public class TaskTable extends JPanel{
 		this.jTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		this.jTable.getColumnModel().getColumn(0).setWidth(50);
 		this.jTable.getTableHeader().setReorderingAllowed(false);
+		
 //		System.out.println("test");
 		
 		
