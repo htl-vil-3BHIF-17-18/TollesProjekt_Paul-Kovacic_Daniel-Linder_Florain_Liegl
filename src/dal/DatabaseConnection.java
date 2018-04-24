1df5a379bf10e9d7d332867a609a76ed56ebfc3f
Daniel Linder <linderd@edu.htl-villach.at>
2018-04-24 13:37:41 +0200

    alles ganz toll neu

--- src/dal/DatabaseConnection.java
+++ src/dal/DatabaseConnection.java
@@ -6,85 +6,114 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.util.*;
+
 import bll.*;
 
 public class DatabaseConnection {
-	private String username;
-	private String password;
+    private String username;
+    private String password;
 
     public DatabaseConnection(String username, String password) {
-		super();
+        super();
         this.username = username;
-		this.password = password;
-	}
+        this.password = password;
+    }
 
-	private Connection createConnection() throws ClassNotFoundException, SQLException {
+    private Connection createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         return DriverManager.getConnection("jdbc:oracle:thin:" + this.username + "/" + this.password + "@192.168.128.152");
     }
-	
-	public List<Task> getAllTasks() {
-		List<Task> tasks = new ArrayList<>();
-		Connection con = null;
-		try {
-			con = this.createConnection();
-			Statement stmtSelect = con.createStatement();
-			ResultSet rs = stmtSelect.executeQuery("SELECT * FROM task");
-			
-			while(rs.next()) {
-				tasks.add(new Task(rs.getString(1) == "Y", Categories.valueOf(rs.getString(2)), Subjects.valueOf(rs.getString(3)), rs.getString(4), rs.getDate(5), rs.getDate(6)));
-			}
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		return tasks;
-	}
-	
-	public List<Task> getFilteredTasks(String tableCol, String filter) {
-		List<Task> tasks = new ArrayList<>();
-		try {
-			Connection con = this.createConnection();
-			Statement stmtSelect = con.createStatement();
-			stmtSelect.executeQuery("SELECT * FROM task WHERE " + tableCol + " LIKE \'" + filter + "\';");
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return tasks;
-	}
-	
-	public void addEntry(Task task) {
-		try {
-			Connection con = this.createConnection();
-			Statement stmtInsert = con.createStatement();
-			stmtInsert.executeQuery("INSERT INTO task VALUES (" + task.toString() + ");");
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void removeEntry(Task task) {
-		try {
-			Connection con = this.createConnection();
-			Statement stmtDelete = con.createStatement();
-			stmtDelete.executeQuery("DELETE * FROM task WHERE category LIKE " + task.getCategorie().toString() + " AND subject LIKE " + task.getSubject().toString() + " AND description LIKE " + task.getDescription() + " AND von = " + task.getFrom() + " AND until = " + task.getUntil() + ";");
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void renewEntry(Task oldTask, Task newTask) {
-		removeEntry(oldTask);
-		addEntry(newTask);
-	}
+
+    public void createTaskTable() {
+        Connection con = null;
+        Statement stmtCreate;
+        try {
+            con = createConnection();
+            stmtCreate = con.createStatement();
+            stmtCreate.execute("CREATE TABLE task (" +
+                    "done VARCHAR2(1)," +
+                    "    category VARCHAR2(20)," +
+                    "    subject VARCHAR2(20)," +
+                    "    description VARCHAR2(50)," +
+                    "    von DATE," +
+                    "    until DATE," +
+                    "    CONSTRAINT pkTask PRIMARY KEY (category, subject, von)," +
+                    "    CONSTRAINT ckDatum CHECK (von <= until)," +
+                    "    CONSTRAINT ckErledigt CHECK (upper(done) LIKE 'Y' OR upper(done) LIKE 'N')" +
+                    ");");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Task> getAllTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Connection con = null;
+        try {
+            con = this.createConnection();
+            Statement stmtSelect = con.createStatement();
+            ResultSet rs = stmtSelect.executeQuery("SELECT * FROM task");
+
+            while (rs.next()) {
+                tasks.add(new Task(rs.getString(1) == "Y", Categories.valueOf(rs.getString(2)), Subjects.valueOf(rs.getString(3)), rs.getString(4), rs.getDate(5), rs.getDate(6)));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return tasks;
+    }
+
+    public List<Task> getFilteredTasks(String tableCol, String filter) {
+        List<Task> tasks = new ArrayList<>();
+        try {
+            Connection con = this.createConnection();
+            Statement stmtSelect = con.createStatement();
+            stmtSelect.executeQuery("SELECT * FROM task WHERE " + tableCol + " LIKE \'" + filter + "\';");
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
+    public void addEntry(Task task) {
+        try {
+            Connection con = this.createConnection();
+            Statement stmtInsert = con.createStatement();
+            stmtInsert.executeQuery("INSERT INTO task VALUES (" + task.toString() + ");");
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void removeEntry(Task task) {
+        try {
+            Connection con = this.createConnection();
+            Statement stmtDelete = con.createStatement();
+            stmtDelete.executeQuery("DELETE * FROM task WHERE category LIKE " + task.getCategorie().toString() + " AND subject LIKE " + task.getSubject().toString() + " AND description LIKE " + task.getDescription() + " AND von = " + task.getFrom() + " AND until = " + task.getUntil() + ";");
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void renewEntry(Task oldTask, Task newTask) {
+        removeEntry(oldTask);
+        addEntry(newTask);
+    }
 }
--- src/gui/LoginDialog.java
+++ src/gui/LoginDialog.java
@@ -17,81 +17,77 @@ import javax.swing.JTextField;
 import bll.Task;
 import dal.DatabaseConnection;
 
-public class LoginDialog extends JDialog implements ActionListener{
+public class LoginDialog extends JDialog implements ActionListener {
 
-	
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 6158225161645311129L;
-	private JLabel lUsername = null;
-	private JTextField tfUsername = null;
-	private JLabel lPassword = null;
-	private JPasswordField pfPasswordField = null;
-	private JButton btnLogin = null;
-	private JButton btnCancel = null;
-	private List<Task> tl=null;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6158225161645311129L;
+    private JLabel lUsername = null;
+    private JTextField tfUsername = null;
+    private JLabel lPassword = null;
+    private JPasswordField pfPasswordField = null;
+    private JButton btnLogin = null;
+    private JButton btnCancel = null;
+    private List<Task> tl = null;
 
-	public LoginDialog(Frame owner, String title, boolean modal) {
-		super(owner, title, modal);
-		this.tl=new ArrayList<Task>();
-		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		this.setMinimumSize(new Dimension(300,130));
-		this.setResizable(false);
-		this.initializeControls();
-		this.pack();
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-	}
+    public LoginDialog(Frame owner, String title, boolean modal) {
+        super(owner, title, modal);
+        this.tl = new ArrayList<>();
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.setMinimumSize(new Dimension(300, 130));
+        this.setResizable(false);
+        this.initializeControls();
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 
-	private void initializeControls() {
-		// TODO Auto-generated method stub
-		GridLayout grid = new GridLayout(3,2);
-		this.setLayout(grid);
-		
-		this.lUsername=new JLabel("Username: ");
-		this.tfUsername=new JTextField();
-		
-		this.lPassword=new JLabel("Password: ");
-		this.pfPasswordField=new JPasswordField();
-		
-		this.btnLogin=new JButton("Login");
-		this.btnCancel=new JButton("Cancel");
-		
-		this.btnLogin.addActionListener(this);
-		this.btnCancel.addActionListener(this);
-		
-		this.add(this.lUsername);
-		this.add(this.tfUsername);
-		this.add(this.lPassword);
-		this.add(this.pfPasswordField);
-		this.add(this.btnLogin);
-		this.add(this.btnCancel);
-	}
+    private void initializeControls() {
+        // TODO Auto-generated method stub
+        GridLayout grid = new GridLayout(3, 2);
+        this.setLayout(grid);
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getSource().equals(this.btnLogin))
-		{
-			
-			DatabaseConnection db =new DatabaseConnection(this.tfUsername.getText(),this.pfPasswordField.getPassword().toString());
-			tl=db.getAllTasks();
-				this.setVisible(false);
-				this.dispose();		
-		}
-		else if(e.getSource().equals(this.btnCancel)){
-			this.setVisible(false);
-			this.dispose();
-		}
-		
-	}
+        this.lUsername = new JLabel("Username: ");
+        this.tfUsername = new JTextField();
 
-	public List<Task> getTl() {
-		return tl;
-	}
-	
-	
+        this.lPassword = new JLabel("Password: ");
+        this.pfPasswordField = new JPasswordField();
+
+        this.btnLogin = new JButton("Login");
+        this.btnCancel = new JButton("Cancel");
+
+        this.btnLogin.addActionListener(this);
+        this.btnCancel.addActionListener(this);
+
+        this.add(this.lUsername);
+        this.add(this.tfUsername);
+        this.add(this.lPassword);
+        this.add(this.pfPasswordField);
+        this.add(this.btnLogin);
+        this.add(this.btnCancel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource().equals(this.btnLogin)) {
+
+            DatabaseConnection db = new DatabaseConnection(this.tfUsername.getText(), this.pfPasswordField.getPassword().toString());
+            tl = db.getAllTasks();
+            this.setVisible(false);
+            this.dispose();
+        } else if (e.getSource().equals(this.btnCancel)) {
+            this.setVisible(false);
+            this.dispose();
+        }
+
+    }
+
+    public List<Task> getTl() {
+        return tl;
+    }
+
 
 }
