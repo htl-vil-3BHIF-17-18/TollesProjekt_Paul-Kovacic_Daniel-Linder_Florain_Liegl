39e041c6ed2bcdfffb5f05736453fd5a0066894f
Daniel Linder <linderd@edu.htl-villach.at>
2018-04-25 16:32:37 +0200

    alles ganz toll neu

--- src/dal/DatabaseConnection.java
+++ src/dal/DatabaseConnection.java
@@ -17,7 +17,7 @@ public class DatabaseConnection {
 
     private Connection createConnection() throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        return DriverManager.getConnection("jdbc:oracle:thin:" + this.username + "/" + this.password + "@192.168.128.152:1521:ora11g");
+        return DriverManager.getConnection("jdbc:oracle:thin:" + this.username + "/" + this.password + "@192.168.128.152:1521:ora11g"); //212.152.179.117
     }
 
     public void createTaskTable() {
