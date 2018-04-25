755b4ea4c0308fc680c504e2c007ef28241606ba
Florian <Florian@FLO-PC>
2018-04-25 18:12:35 +0200

    sd

--- src/gui/TaskDialog.java
+++ src/gui/TaskDialog.java
@@ -7,7 +7,6 @@ import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
 
-
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,171 +17,159 @@ import java.util.Calendar;
 import java.util.Properties;
 import javax.swing.*;
 
+public class TaskDialog extends JDialog implements ActionListener {
 
+	private JLabel lbCategory = null;
+	private JComboBox JCategory = null;
+	private JLabel lbSubject = null;
+	private JComboBox JSubject = null;
+	private JLabel lbDescription = null;
+	private JTextField tfDescription = null;
+	private JLabel lbDateFrom = null;
+	private JFormattedTextField tfFrom = null;
+	private JLabel lbto = null;
+	private JFormattedTextField tfto = null;
+	private JButton btnOk = null;
+	private JButton btnCancel = null;
+	private Task task = null;
+	JDatePickerImpl datePicker;
+	JDatePickerImpl datePickerTo;
 
-public class TaskDialog extends JDialog  implements ActionListener{
+	JDatePanelImpl datePanelFrom;
+	JDatePanelImpl datePanelTo;
+	private JPanel panel = null;
 
-    private JLabel lbCategory = null;
-    private JComboBox JCategory = null;
-    private JLabel lbSubject = null;
-    private JComboBox JSubject = null;
-    private JLabel lbDescription = null;
-    private JTextField tfDescription = null;
-    private JLabel lbDateFrom = null;
-    private JFormattedTextField  tfFrom= null;
-    private JLabel lbto = null;
-    private JFormattedTextField  tfto= null;
-    private JButton btnOk = null;
-    private JButton btnCancel = null;
-    private Task task= null;
-    JDatePickerImpl datePicker;
-    JDatePickerImpl datePickerTo;
+	public TaskDialog(Frame owner, String title, boolean modal) {
+		super(owner, title, modal);
+		this.initializeControls();
+		this.pack();
+		this.setVisible(true);
+	}
 
-    JDatePanelImpl datePanelFrom;
-    JDatePanelImpl datePanelTo;
-    private JPanel panel = null;
+	private void initializeControls() {
+		GridLayout grid = new GridLayout(6, 2);
+		this.setLayout(grid);
+		Categories[] Categoriesvalues = Categories.values();
+		this.JCategory = new JComboBox(Categoriesvalues);
+		this.lbCategory = new JLabel("Category:");
 
+		Subjects[] Subjectvalues = Subjects.values();
+		this.JSubject = new JComboBox(Subjectvalues);
+		this.lbSubject = new JLabel("Subject:");
 
+		this.lbDescription = new JLabel("Description:");
+		this.tfDescription = new JTextField();
 
-    public TaskDialog(Frame owner, String title, boolean modal)
-    {
-        super(owner, title, modal);
-        this.initializeControls();
-        this.pack();
-        this.setVisible(true);
-    }
+		this.lbDateFrom = new JLabel("From:");
+		SimpleDateFormat time = new SimpleDateFormat("dd.mm.yyyy");
+		this.tfFrom = new JFormattedTextField(time);
 
-    private void initializeControls() {
-        GridLayout grid = new GridLayout(6, 2);
-        this.setLayout(grid);
-        Categories[] Categoriesvalues=Categories.values();
-        this.JCategory=new JComboBox(Categoriesvalues);
-        this.lbCategory=new JLabel("Category:");
+		this.lbto = new JLabel("To:");
+		this.tfto = new JFormattedTextField(time);
 
-        Subjects[] Subjectvalues=Subjects.values();
-        this.JSubject=new JComboBox(Subjectvalues);
-        this.lbSubject=new JLabel("Subject:");
+		this.btnOk = new JButton("OK");
+		this.btnOk.addActionListener(this);
 
-        this.lbDescription=new JLabel("Description:");
-        this.tfDescription=new JTextField();
+		this.btnCancel = new JButton("Cancel");
+		this.btnCancel.addActionListener(this);
 
-        this.lbDateFrom=new JLabel("From:");
-        SimpleDateFormat time=new SimpleDateFormat("dd.mm.yyyy");
-        this.tfFrom=new JFormattedTextField(time);
+		UtilDateModel modelFrom = new UtilDateModel();
+		modelFrom.setValue(java.util.Calendar.getInstance().getTime());
+		modelFrom.setSelected(true);
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		datePanelFrom = new JDatePanelImpl(modelFrom, p);
+		datePicker = new JDatePickerImpl(datePanelFrom, new DateLabelFormatter());
 
-        this.lbto=new JLabel("To:");
-        this.tfto=new JFormattedTextField(time);
+		UtilDateModel modelTo = new UtilDateModel();
+		modelTo.setValue(java.util.Calendar.getInstance().getTime());
+		modelFrom.setSelected(true);
+		datePanelTo = new JDatePanelImpl(modelTo, p);
+		datePickerTo = new JDatePickerImpl(datePanelTo, new DateLabelFormatter());
 
-        this.btnOk = new JButton("OK");
-        this.btnOk.addActionListener(this);
+		this.add(this.lbCategory);
+		this.add(this.JCategory);
+		this.add(this.lbSubject);
+		this.add(this.JSubject);
+		this.add(this.lbDescription);
+		this.add(this.tfDescription);
+		this.add(this.lbDateFrom);
+		this.add(this.datePicker);
+		this.add(this.lbto);
+		this.add(this.datePickerTo);
+		this.add(this.btnOk);
+		this.add(this.btnCancel);
+	}
 
-        this.btnCancel = new JButton("Cancel");
-        this.btnCancel.addActionListener(this);
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource().equals(this.btnOk)) {
 
+			if (this.writeValuesToMenu()) {
+				this.setVisible(false);
+				this.dispose();
+			}
 
-        UtilDateModel modelFrom= new UtilDateModel();
-        modelFrom.setValue(java.util.Calendar.getInstance().getTime());
-        modelFrom.setSelected(true);
-        Properties p = new Properties();
-        p.put("text.today", "Today");
-        p.put("text.month", "Month");
-        p.put("text.year", "Year");
-        datePanelFrom = new JDatePanelImpl(modelFrom,p);
-        datePicker = new JDatePickerImpl(datePanelFrom, new DateLabelFormatter());
+		} else if (e.getSource().equals(this.btnCancel)) {
+			this.setVisible(false);
+			this.dispose();
+		}
 
-        UtilDateModel modelTo= new UtilDateModel();
-        modelTo.setValue(java.util.Calendar.getInstance().getTime());
-        modelFrom.setSelected(true);
-        datePanelTo = new JDatePanelImpl(modelTo,p);
-        datePickerTo = new JDatePickerImpl(datePanelTo, new DateLabelFormatter());
+	}
 
-        this.add(this.lbCategory);
-        this.add(this.JCategory);
-        this.add(this.lbSubject);
-        this.add(this.JSubject);
-        this.add(this.lbDescription);
-        this.add(this.tfDescription);
-        this.add(this.lbDateFrom);
-        this.add(this.datePicker);
-        this.add(this.lbto);
-        this.add(this.datePickerTo);
-        this.add(this.btnOk);
-        this.add(this.btnCancel);
-    }
+	private boolean writeValuesToMenu() {
+		boolean isValid = false;
 
+		try {
+			if (!this.tfDescription.getText().trim().isEmpty()
+					&& !this.datePicker.getJFormattedTextField().getText().trim().isEmpty()
+					&& !this.datePickerTo.getJFormattedTextField().getText().trim().isEmpty()) {
+				SimpleDateFormat time = new SimpleDateFormat("dd.MM.yyyy");
 
+				isValid = true;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if (e.getSource().equals(this.btnOk)) {
+				this.task = new Task(false, Categories.values()[this.JCategory.getSelectedIndex()],
+						Subjects.values()[this.JSubject.getSelectedIndex()], this.tfDescription.getText(),
+						time.parse(this.datePicker.getJFormattedTextField().getText()),
+						time.parse(this.datePickerTo.getJFormattedTextField().getText()));
+				System.out.println(this.task.toString());
+			}
 
-            if (this.writeValuesToMenu()) {
-                this.setVisible(false);
-                this.dispose();
-            }
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+		}
 
-        } else if (e.getSource().equals(this.btnCancel)) {
-            this.setVisible(false);
-            this.dispose();
-        }
+		return isValid;
 
-    }
-
-    private boolean writeValuesToMenu() {
-        boolean isValid = false;
-
-        try {
-            if (!this.tfDescription.getText().trim().isEmpty() && !this.datePicker.getJFormattedTextField().getText().trim().isEmpty() && !this.datePickerTo.getJFormattedTextField().getText().trim().isEmpty()) {
-               SimpleDateFormat time=new SimpleDateFormat("dd.MM.yyyy");
-
-
-               isValid = true;
-
-               this.task= new Task(false,Categories.values()[this.JCategory.getSelectedIndex()],Subjects.values()[this.JSubject.getSelectedIndex()],this.tfDescription.getText(), time.parse(this.datePicker.getJFormattedTextField().getText()),time.parse(this.datePickerTo.getJFormattedTextField().getText()));
-               System.out.println(this.task.toString());
-            }
-
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return isValid;
-
-    }
+	}
 
 	public Task getTask() {
 		return task;
 	}
-    
-    
 
 }
 
+class DateLabelFormatter extends JFormattedTextField.AbstractFormatter {
 
+	private String datePattern = "dd.MM.yyyy";
+	private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
 
- class DateLabelFormatter extends JFormattedTextField.AbstractFormatter
-{
+	@Override
+	public Object stringToValue(String text) throws ParseException {
+		return dateFormatter.parseObject(text);
+	}
 
-    private String datePattern = "dd.MM.yyyy";
-    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
+	@Override
+	public String valueToString(Object value) throws ParseException {
+		if (value != null) {
+			Calendar cal = (Calendar) value;
+			return dateFormatter.format(cal.getTime());
+		}
 
-    @Override
-    public Object stringToValue(String text) throws ParseException
-    {
-        return dateFormatter.parseObject(text);
-    }
-
-    @Override
-    public String valueToString(Object value) throws ParseException
-    {
-        if (value != null)
-        {
-            Calendar cal = (Calendar) value;
-            return dateFormatter.format(cal.getTime());
-        }
-
-        return "";
-    }
+		return "";
+	}
 
 }
