7be2eb0d231aac4aba75f5e9a6ff49f1fbbf2263
Florian <Florian@FLO-PC>
2018-04-26 08:26:31 +0200

    date

--- src/gui/MyTableModel.java
+++ src/gui/MyTableModel.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
@@ -11,6 +12,9 @@ public class MyTableModel extends DefaultTableModel {
 	 */
 	private static final long serialVersionUID = -2412407473703651652L;
 	private String[] columnNames = { "DONE", "Category", "Subject", "Description", "From", "Until" };
+	SimpleDateFormat f = new SimpleDateFormat("dd.MM.yyyy");
+	Date d= new Date();
+	
 
 //	private Object[][] data= {{null,null,null,null,null,null}};
 	
@@ -28,9 +32,36 @@ public class MyTableModel extends DefaultTableModel {
 	public Class<?> getColumnClass(int columnIndex) {
 	    if (columnIndex == 0)
 	        return Boolean.class;
+
 	    return super.getColumnClass(columnIndex);
+	    
 	}
 	
+	
+	
+	@Override
+	public void setValueAt(Object aValue, int row, int column) {
+		// TODO Auto-generated method stub
+		if( aValue instanceof Date) {
+			aValue = f.format(aValue);
+        }
+		super.setValueAt(aValue, row, column);
+	}
+	
+	
+
+
+
+	@Override
+	public void addRow(Object[] rowData) {
+		// TODO Auto-generated method stub
+		rowData[4]=f.format(rowData[4]);
+		rowData[5]=f.format(rowData[5]);
+		super.addRow(rowData);
+	}
+
+
+
 	@Override
 	public boolean isCellEditable(int row, int col) {
 	    return (col == 0); 
@@ -40,7 +71,7 @@ public class MyTableModel extends DefaultTableModel {
         return columnNames[col];
     }
 
-
+	
 	
 	
 
