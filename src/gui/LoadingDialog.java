e02eb255e4c19417a27b309f93168bc739036dca
Daniel Linder <linderd@edu.htl-villach.at>
2018-04-28 16:55:14 +0200

    alles ganz toll neu

--- src/gui/LoadingDialog.java
+++ src/gui/LoadingDialog.java
@@ -3,22 +3,20 @@ package gui;
 import javax.swing.*;
 import java.awt.*;
 
-public class LoadingDialog extends JDialog {
-    private JLabel loading = null;
-    private JProgressBar jpb = null;
+class LoadingDialog extends JDialog {
 
 
-    public LoadingDialog(Dialog owner, String title, boolean modal) {
+    LoadingDialog(Dialog owner, String title, boolean modal) {
         super(owner, title, modal);
         initializeControls();
     }
 
     private void initializeControls() {
         this.setLayout(new GridLayout(1, 1));
-        this.jpb = new JProgressBar();
+        JProgressBar jpb = new JProgressBar();
         this.add(jpb);
-        this.jpb.setIndeterminate(true);
-        this.jpb.setSize(new Dimension(250, 20));
+        jpb.setIndeterminate(true);
+        jpb.setSize(new Dimension(250, 20));
         this.setMinimumSize(new Dimension(300, 60));
         this.setLocationRelativeTo(null);
         this.setVisible(true);
